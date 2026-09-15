3ffabdf9068ae9a62a3ff935168bd5648e7d31a6
Maxence-Santos <maxence1405@free.fr>
2026-09-15 15:30:51 +0200

    Question 3 : tuples pour le journal de bord du robot

---
+++ tp0/tuples.py
@@ -0,0 +1,25 @@
+def afficher_releve(tuple):
+    texte = f"Capteur {tuple[0]} : {str(tuple[1])} {tuple[2]}"
+    return texte
+
+def recalibrer(liste, capteur, val):
+    for i in range(len(liste)-1):
+        liste[i] = list(liste[i])
+        if liste[i][0] == capteur:
+            liste[i][1] = val
+            liste[i] = tuple(liste[i])
+        break
+    return liste
+
+releve1 = ("laser_avant", 2.35, "m")
+releve2 = ("laser_arriere", 1.10, "m")
+releve3 = ("gyroscope", 87.5, "deg")
+releves = [releve1, releve2, releve3]
+assert len(releves) == 3
+assert releves[0][0] == "laser_avant"
+assert afficher_releve(releve1) == "Capteur laser_avant : 2.35 m"
+
+nouveaux_releves = recalibrer(releves, "laser_avant", 2.40)
+assert nouveaux_releves[0] == ("laser_avant", 2.40, "m")
+assert nouveaux_releves[1] == releve2
+assert nouveaux_releves[2] == releve3
